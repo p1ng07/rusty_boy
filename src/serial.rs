7359acc9b4e38a3a5923b02f847966261b3ce2b5
p1ng07 <franciscoppontes369@gmail.com>
2023-02-27 10:57:04 +0000

    Fixed serial output to console, 01-special.gb passes

--- src/serial.rs
+++ src/serial.rs
@@ -2,10 +2,17 @@
 pub struct Serial {
     pub serial_data_transfer: u8,
     pub serial_data_control: u8,
+
+    pub current_word: String,
 }
 
 impl Serial {
     pub fn write_to_transfer(&mut self, data: u8) {
-        println!("a{}", data as char)
+	if data as char == '\n' {
+	    println!("{}", self.current_word);
+	    self.current_word = String::from("");
+	} else {
+	    self.current_word.push(data as char);
+	}
     }
 }
