5c289ba0df9f6f348a55f9db7a27f4e199037310
p1ng07 <franciscoppontes369@gmail.com>
2023-06-05 00:30:26 +0100

    Added mbc1 support

--- src/mbc/mbc1.rs
+++ src/mbc/mbc1.rs
@@ -1,21 +1,128 @@
-use super::Mbc;
+use std::ops::{Mul, Add, Index, BitAnd};
+
+use super::{Mbc, no_mbc::KIBI_BYTE};
 
 pub struct Mbc1 {
-    
+    ram_enabled: bool,
+    ram_bank_index: usize,
+    rom_bank_index: usize,
+    rom_bank_mask: u16, // Used to mask the value written to the rom bank register
+    rom_banks: Vec<[u8; 16 * KIBI_BYTE]>,
+    ram_banks: Option<Vec<[u8; 8 * KIBI_BYTE]>>
 }
 
 impl Mbc for Mbc1 {
     fn read_byte(&self, address: u16) -> u8 {
-        todo!()
+	match address {
+	    ..=0x3FFF => self.rom_banks[0][address as usize], // Reading rom bank 0
+	    0x4000..=0x7FFF => {
+		match self.rom_banks.get(self.rom_bank_index) { // Reading rom bank 01-7F
+		    Some(rom_bank) => rom_bank[address as usize - 0x4000],
+		    None => 0xFF
+		}
+	    },
+	    0xA000..=0xBFFF => { // Reading ram bank 00-04
+		if !self.ram_enabled { return 0xFF; }
+
+		if let Some(ref vector) = self.ram_banks {
+		    vector[self.ram_bank_index][address as usize - 0xA000]
+		}else {
+		    0xFF
+		}
+	    },
+	    _ => 0xFF
+	}
     }
 
     fn write_byte(&mut self, address: u16, byte: u8) {
-        todo!()
+	match address{
+	    ..=0x1FFF => {
+		if byte.bitand(0xF) == 0xA {
+		    self.ram_enabled = true;
+		} else {
+		    self.ram_enabled = false;
+		}
+	    }
+	    0x2000..=0x3FFF =>{
+		self.rom_bank_index = self.rom_bank_mask.bitand(byte as u16) as usize;
+		if self.rom_bank_index == 0 {
+		    self.rom_bank_index = 1;
+		}
+	    }
+	    0x4000..=0x5FFF => {
+		self.ram_bank_index = byte.bitand(2) as usize;
+	    }
+	    _ => ()
+	}
     }
 }
 
 impl Mbc1 {
     pub fn new(total_rom: Vec<u8>) -> Self {
-	Self{}
+
+	let num_of_banks = (32 * KIBI_BYTE) * ((1 as usize) << total_rom[0x148] as usize);
+
+	let mut cartridge_total_iterator = 0usize;
+
+	let mut rom_banks: Vec<[u8; 16 * KIBI_BYTE]> = Vec::with_capacity(num_of_banks);
+	let rom_bank_mask = match num_of_banks {
+	    2 => 1u16,
+	    4 => 2u16,
+	    8 => 3u16,
+	    16 => 4u16,
+	    32 => 5u16,
+	    _ => panic!("{} is not a valid rom bank number.", num_of_banks)
+	};
+
+	// Copy every rom bank on the cartridge
+	// Every bank is comprised of 16 KiB
+	for _ in 0..num_of_banks - 1 {
+	    let mut new_vec: [u8; 16 * KIBI_BYTE] = [0; 16 * KIBI_BYTE];
+	    for j in 0..(16 * KIBI_BYTE) - 1 {
+		new_vec[j] = match total_rom.get(cartridge_total_iterator) {
+		    Some(x) => {
+			cartridge_total_iterator += 1;
+			x.to_owned()
+		    }
+		    None => 0xFF,
+		};
+	    }
+	    rom_banks.push(new_vec);
+	}
+
+	// Initialize ram based on the size given in the rom
+	let mut ram_banks = match total_rom[0x149] {
+	    0 | 1 => None,
+	    2 => Some([[0u8; 8 * KIBI_BYTE]; 1].to_vec()), // 1 bank of 8 KiB
+	    3 => Some([[0u8; 8 * KIBI_BYTE]; 4].to_vec()), // 4 banks of 8 KiB
+	    _ => panic!("Ram size specified on the cartridge isn't available on this mbc.")
+	};
+
+	// Populate external ram
+	match ram_banks.as_mut(){
+	    Some(array) => {
+		for i in 0..array.len() {
+		    for j in 0..array[0].len() - 1 {
+			(*array)[i][j] = match total_rom.get(cartridge_total_iterator){
+			    Some(x) => {
+				cartridge_total_iterator += 1;
+				x.to_owned()
+			    },
+			    None => 0
+			};
+		    };
+		};
+	    },
+	    None => ()
+	}
+
+	Self{
+	    ram_enabled: false,
+	    ram_bank_index: 0,
+	    rom_bank_index: 1,
+	    rom_bank_mask,
+	    rom_banks,
+	    ram_banks,
+	}
     }
 }
